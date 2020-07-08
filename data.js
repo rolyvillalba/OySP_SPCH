3f17f02b4c0551474fbba6e8df10311179191297
rolyvillalba <rolyvillalba@gmail.com>
2020-07-08 19:28:35 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,374 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-calle-arroyo-covunco",
+      "name": "CALLE ARROYO COVUNCO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.16596324736570622,
+        "pitch": 0.06021570429404832,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2133291924962748,
+          "pitch": 0.12479118693716451,
+          "rotation": 0,
+          "target": "2-recepcion"
+        },
+        {
+          "yaw": -1.3928829473585616,
+          "pitch": 0.023270642895548477,
+          "rotation": 1.5707963267948966,
+          "target": "6-playa"
+        },
+        {
+          "yaw": -1.5103886751055118,
+          "pitch": -0.0016587668706371517,
+          "rotation": 1.5707963267948966,
+          "target": "1-calle-de-servicio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-calle-de-servicio",
+      "name": "CALLE DE SERVICIO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.1819860921924956,
+        "pitch": 0.015655489283615154,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15680814694846212,
+          "pitch": 0.01535327443687251,
+          "rotation": 4.71238898038469,
+          "target": "0-calle-arroyo-covunco"
+        },
+        {
+          "yaw": -2.4857039122726867,
+          "pitch": 0.01031983693862415,
+          "rotation": 1.5707963267948966,
+          "target": "6-playa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-recepcion",
+      "name": "RECEPCION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.1478170690037004,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3442998217265334,
+          "pitch": 0.1595902597221528,
+          "rotation": 10.995574287564278,
+          "target": "3-auxiliares"
+        },
+        {
+          "yaw": 0.4691404892659339,
+          "pitch": 0.04747740846217674,
+          "rotation": 0,
+          "target": "6-playa"
+        },
+        {
+          "yaw": -1.2517614617661224,
+          "pitch": 0.2967567686634993,
+          "rotation": 4.71238898038469,
+          "target": "0-calle-arroyo-covunco"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9726947348440138,
+          "pitch": -0.13502984131556417,
+          "title": "a Servicios Públicos",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.342414274875118,
+          "pitch": -0.10905739916413104,
+          "title": "a Obras Públicas",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-auxiliares",
+      "name": "AUXILIARES",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.06875308527518342,
+        "pitch": 0.10765142245215742,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.40290950207922016,
+          "pitch": 0.14702784398082436,
+          "rotation": 0,
+          "target": "4-area-tecnica"
+        },
+        {
+          "yaw": 2.3352499945277065,
+          "pitch": 0.011416897820264893,
+          "rotation": 10.995574287564278,
+          "target": "6-playa"
+        },
+        {
+          "yaw": 2.7574976734911845,
+          "pitch": 0.05199835928180718,
+          "rotation": 0,
+          "target": "2-recepcion"
+        },
+        {
+          "yaw": 2.825514660013603,
+          "pitch": 0.190164335760457,
+          "rotation": 1.5707963267948966,
+          "target": "5-sala-de-reuniones"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-area-tecnica",
+      "name": "AREA TECNICA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.00410711054948365,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5574935935400234,
+          "pitch": 0.1835334038364067,
+          "rotation": 1.5707963267948966,
+          "target": "3-auxiliares"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sala-de-reuniones",
+      "name": "SALA DE REUNIONES",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.6958035916709182,
+        "pitch": 0.02760260979808571,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3541140994812686,
+          "pitch": 0.08455372429643582,
+          "rotation": 4.71238898038469,
+          "target": "2-recepcion"
+        },
+        {
+          "yaw": 2.023999480216915,
+          "pitch": 0.2310658957362186,
+          "rotation": 1.5707963267948966,
+          "target": "3-auxiliares"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-playa",
+      "name": "PLAYA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.225995783458913,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.186091220537163,
+          "pitch": 0.06746414771472864,
+          "rotation": 0,
+          "target": "3-auxiliares"
+        },
+        {
+          "yaw": -2.6317551208679113,
+          "pitch": 0.03998408513587215,
+          "rotation": 0,
+          "target": "2-recepcion"
+        },
+        {
+          "yaw": 0.13656224270457784,
+          "pitch": 0.055356537704785325,
+          "rotation": 0,
+          "target": "7-comedor"
+        },
+        {
+          "yaw": -0.8227497648846924,
+          "pitch": 0.017552423494443303,
+          "rotation": 10.995574287564278,
+          "target": "0-calle-arroyo-covunco"
+        },
+        {
+          "yaw": 1.0174724723491622,
+          "pitch": -0.014358207505228648,
+          "rotation": 4.71238898038469,
+          "target": "1-calle-de-servicio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-comedor",
+      "name": "COMEDOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.1122980532321378,
+        "pitch": 0.047677235105787474,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8973229426065181,
+          "pitch": 0.05809377779863745,
+          "rotation": 1.5707963267948966,
+          "target": "6-playa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Nuevas Oficinas OySP SPCH",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
